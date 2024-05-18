0ff17f00a9028235554180b81b5751b3903bf0b8
selinakudadze <kudadzeselina@gmail.com>
2024-05-18 11:42:36 +0000

    Second commit to the repository

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -17,4 +17,27 @@ function processArray(arrNum){
 console.log(processArray([1,2,3,4]));
 
 
+//TASK 2
+function formatArrayStrings(arrStrings,processedArray) {
+    for(let i=0; i<arrStrings.length; i++) {
+        if(processedArray[i]%2 === 0) {
+            arrStrings[i] = arrStrings[i].toUpperCase();
+    }else{
+        arrStrings[i] = arrStrings[i].toLowerCase();
+    }
+    }
+    return arrStrings;
+}
+
+//SAMPLE 2
+let arrayStrings=['hi','good','bad'];
+let processedNumbers=processArray([1,2,5]);
+
+console.log(formatArrayStrings(arrayStrings,processedNumbers));
+
+module.exports ={formatArrayStrings,processArray};
+
+
+
+
 
